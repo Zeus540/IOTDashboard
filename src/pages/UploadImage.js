6fea056e4f66d40271ea7374de86de9fb77cdb4c
Zaheer <zaheerroberts4@gmail.com>
2022-10-05 10:17:40 +0200

    add upload

--- src/pages/UploadImage.js
+++ src/pages/UploadImage.js
@@ -1,33 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Webcam from "react-webcam";
 const videoConstraints = {
     width: 1280,
     height: 720,
-    facingMode: "user"
+    facingMode: { exact: "environment" }
   };
   
   const UploadImage = () => {
     const webcamRef = React.useRef(null);
 
+    const [imgData, setImgData] = useState("")
+
     const capture = React.useCallback(
       () => {
         const imageSrc = webcamRef.current.getScreenshot();
         console.log("imageSrc",imageSrc)
+        setImgData(imageSrc)
       },
       [webcamRef]
     );
 
     return (
       <>
+   
         <Webcam
           audio={false}
           height={720}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
-          width={1280}
+          width="100%"
           videoConstraints={videoConstraints}
         />
         <button onClick={capture}>Capture photo</button>
+        <img src={imgData}  />
       </>
     );
   };
