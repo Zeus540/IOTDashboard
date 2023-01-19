db2fa136ff8a1938f7a249e51a826846fa3c6f09
Zeus <zaheerroberts4@gmail.com>
2023-01-20 01:07:48 +0200

    updated

--- src/components/shared/Constants.js
+++ src/components/shared/Constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL_LOCAL = "http://localhost:9954"
 
 export const BASE_URL_PROD = "https://api.sweetleaf.co.za"
-export const BASE_URL_PROD_SOCKET= "http://api.sweetleaf.co.za:9954"
+export const BASE_URL_PROD_SOCKET= "https://api.sweetleaf.co.za"
